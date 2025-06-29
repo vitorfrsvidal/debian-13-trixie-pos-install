d17ae23cacd21f545788f6e445f74113e2d47653
Vitor Vidal <87544179+vitorfrsvidal@users.noreply.github.com>
2025-06-29 15:01:03 -0300

    Update and rename nvidia_570.158.01-1_driver_install.sh to debian-13-trixie-pos-install.sh

---
+++ debian-13-trixie-pos-install.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+wget https://developer.download.nvidia.com/compute/cuda/repos/debian12/x86_64/cuda-keyring_1.1-1_all.deb && dpkg -i cuda-keyring_1.1-1_all.deb && apt update && apt install linux-headers-amd64 && apt install libxnvctrl0:amd64=570.158.01-1 libnvidia-ptxjitcompiler1:amd64=570.158.01-1 libnvidia-pkcs11-openssl3:amd64=570.158.01-1 nvidia-settings:amd64=570.158.01-1 nvidia-xconfig:amd64=570.158.01-1 libnvidia-nvvm4:amd64=570.158.01-1 libcuda1:amd64=570.158.01-1 nvidia-persistenced:amd64=570.158.01-1 nvidia-opencl-icd:amd64=570.158.01-1 libcudadebugger1:amd64=570.158.01-1 libnvcuvid1:amd64=570.158.01-1 libnvidia-fbc1:amd64=570.158.01-1 libnvoptix1:amd64=570.158.01-1 libnvidia-encode1:amd64=570.158.01-1 libnvidia-opticalflow1:amd64=570.158.01-1 libnvidia-sandboxutils:amd64=570.158.01-1 nvidia-driver-cuda:amd64=570.158.01-1 nvidia-modprobe:amd64=570.158.01-1 xserver-xorg-video-nvidia:amd64=570.158.01-1 libegl-nvidia0:amd64=570.158.01-1 nvidia-vdpau-driver:amd64=570.158.01-1 libnvidia-ngx1:amd64=570.158.01-1 libnvidia-cfg1:amd64=570.158.01-1 libnvidia-allocator1:amd64=570.158.01-1 libnvidia-glvkspirv:amd64=570.158.01-1 libnvidia-ml1:amd64=570.158.01-1 libnvidia-glcore:amd64=570.158.01-1 libnvidia-gpucomp:amd64=570.158.01-1 libglx-nvidia0:amd64=570.158.01-1 libnvidia-api1:amd64=570.158.01-1 libnvidia-eglcore:amd64=570.158.01-1 nvidia-vulkan-icd:amd64=570.158.01-1 nvidia-egl-icd:amd64=570.158.01-1 libnvidia-vksc-core:amd64=570.158.01-1 nvidia-driver-libs:amd64=570.158.01-1 libgles-nvidia2:amd64=570.158.01-1 libgles-nvidia1:amd64=570.158.01-1 libnvidia-rtcore:amd64=570.158.01-1 nvidia-driver-libs:amd64=570.158.01-1 nvidia-kernel-support:amd64=570.158.01-1 nvidia-driver:amd64=570.158.01-1 firmware-nvidia-gsp:amd64=570.158.01-1 nvidia-kernel-open-dkms:amd64=570.158.01-1 nvidia-open-570.158.01 && echo "Driver Nvidia 570.158.01-1 instalado!" || exit 1
+#apt install nvidia-open-570
+#apt install cuda-toolkit-12
+
+# PS.: Ficar de olho pra ver quando vai aparecer o repositório debian13
+
+sleep 3
+
+apt update && apt install r8125-dkms && echo "Driver r8125 instalado!" || exit 1
+
+sleep 3
+
+# Alternativa mais "soft"
+echo "blacklist r8169" | sudo tee /etc/modprobe.d/r8169-disable.conf && sudo update-initramfs -u -k all && echo "Driver r8169 blacklistado! Reinicie o computador..." || exit 1
+
+# Alternativa mais "hard"
+#echo "install r8169 /bin/false" | sudo tee /etc/modprobe.d/r8169-disable.conf && update-initramfs -u -k all && echo "Driver r8169 blacklistado! Reinicie o computador..." || exit 1
+
+# Alternativa mais "completa"
+#echo -e "blacklist r8169\ninstall r8169 /bin/false" | sudo tee /etc/modprobe.d/r8169-disable.conf && sudo update-initramfs -u -k all && echo "Driver r8169 blacklistado! Reinicie o computador..." || exit 1
+
