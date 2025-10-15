111263123f771845461d2db040583b7e187400a1
Vitor Vidal <87544179+vitorfrsvidal@users.noreply.github.com>
2025-10-15 12:48:42 -0300

    Update debian-13-trixie-pos-install.sh
    Tested and approved in TUF GAMING X870-PLUS WIFI and RTX 5090

--- debian-13-trixie-pos-install.sh
+++ debian-13-trixie-pos-install.sh
@@ -1,23 +1,47 @@
 #!/bin/bash
 
-wget https://developer.download.nvidia.com/compute/cuda/repos/debian12/x86_64/cuda-keyring_1.1-1_all.deb && dpkg -i cuda-keyring_1.1-1_all.deb && apt update && apt install linux-headers-amd64 && apt install libxnvctrl0:amd64=570.158.01-1 libnvidia-ptxjitcompiler1:amd64=570.158.01-1 libnvidia-pkcs11-openssl3:amd64=570.158.01-1 nvidia-settings:amd64=570.158.01-1 nvidia-xconfig:amd64=570.158.01-1 libnvidia-nvvm4:amd64=570.158.01-1 libcuda1:amd64=570.158.01-1 nvidia-persistenced:amd64=570.158.01-1 nvidia-opencl-icd:amd64=570.158.01-1 libcudadebugger1:amd64=570.158.01-1 libnvcuvid1:amd64=570.158.01-1 libnvidia-fbc1:amd64=570.158.01-1 libnvoptix1:amd64=570.158.01-1 libnvidia-encode1:amd64=570.158.01-1 libnvidia-opticalflow1:amd64=570.158.01-1 libnvidia-sandboxutils:amd64=570.158.01-1 nvidia-driver-cuda:amd64=570.158.01-1 nvidia-modprobe:amd64=570.158.01-1 xserver-xorg-video-nvidia:amd64=570.158.01-1 libegl-nvidia0:amd64=570.158.01-1 nvidia-vdpau-driver:amd64=570.158.01-1 libnvidia-ngx1:amd64=570.158.01-1 libnvidia-cfg1:amd64=570.158.01-1 libnvidia-allocator1:amd64=570.158.01-1 libnvidia-glvkspirv:amd64=570.158.01-1 libnvidia-ml1:amd64=570.158.01-1 libnvidia-glcore:amd64=570.158.01-1 libnvidia-gpucomp:amd64=570.158.01-1 libglx-nvidia0:amd64=570.158.01-1 libnvidia-api1:amd64=570.158.01-1 libnvidia-eglcore:amd64=570.158.01-1 nvidia-vulkan-icd:amd64=570.158.01-1 nvidia-egl-icd:amd64=570.158.01-1 libnvidia-vksc-core:amd64=570.158.01-1 nvidia-driver-libs:amd64=570.158.01-1 libgles-nvidia2:amd64=570.158.01-1 libgles-nvidia1:amd64=570.158.01-1 libnvidia-rtcore:amd64=570.158.01-1 nvidia-driver-libs:amd64=570.158.01-1 nvidia-kernel-support:amd64=570.158.01-1 nvidia-driver:amd64=570.158.01-1 firmware-nvidia-gsp:amd64=570.158.01-1 nvidia-kernel-open-dkms:amd64=570.158.01-1 nvidia-open-570.158.01 && echo "Driver Nvidia 570.158.01-1 instalado!" || exit 1
-#apt install nvidia-open-570
-#apt install cuda-toolkit-12
+apt update && apt install build-essential vim gedit lm-sensors psensor gparted smartmontools kolourpaint linux-headers-amd64 easyeffects rsync gnome-shell-extension-manager auditd audispd-plugins
 
-# PS.: Ficar de olho pra ver quando vai aparecer o repositório debian13
+# PS.: Ficar de olho para ver quando vai aparecer o repositorio debian13 com os drivers da nvidia
+sleep 3
+wget https://developer.download.nvidia.com/compute/cuda/repos/debian12/x86_64/cuda-keyring_1.1-1_all.deb && dpkg -i cuda-keyring_1.1-1_all.deb && apt update && apt install nvidia-open && echo "driver nvidia-open instalado!" || exit 1
+
+#sleep 3
+#apt update && apt install cuda-toolkit && echo "cuda-toolkit instalado!" || exit 1
+
+
+# ------------------------------------------------------------------------------------------------------
+
+
+# Instalar kernel backported (verificar se os repositorios de backports estão ativos em /etc/apt/sources.list)
 
 sleep 3
+apt update && apt install -t trixie-backports linux-image-amd64 linux-headers-amd64 && echo "backported kernel instalado!" || exit 1
 
-apt update && apt install r8125-dkms && echo "Driver r8125 instalado!" || exit 1
 
-sleep 3
+# ------------------------------------------------------------------------------------------------------
 
-# Alternativa mais "soft"
-echo "blacklist r8169" | sudo tee /etc/modprobe.d/r8169-disable.conf && sudo update-initramfs -u -k all && echo "Driver r8169 blacklistado! Reinicie o computador..." || exit 1
+# Instalar o driver r8125 e blacklistar o driver r8169
 
-# Alternativa mais "hard"
+
+#sleep 3
+#apt update && apt install r8125-dkms && echo "Driver r8125 instalado!" || exit 1
+
+
+# Alternativa mais "soft":
+
+#sleep 3
+#echo "blacklist r8169" | sudo tee /etc/modprobe.d/r8169-disable.conf && sudo update-initramfs -u -k all && echo "Driver r8169 blacklistado! Reinicie o computador..." || exit 1
+
+
+# Alternativa mais "hard" (utilizar se a "soft" nao funcionar):
+
+#sleep 3 
 #echo "install r8169 /bin/false" | sudo tee /etc/modprobe.d/r8169-disable.conf && update-initramfs -u -k all && echo "Driver r8169 blacklistado! Reinicie o computador..." || exit 1
 
-# Alternativa mais "completa"
+
+# Alternativa mais "completa" (utilizar se a "hard" nao funcionar):
+
+#sleep 3 
 #echo -e "blacklist r8169\ninstall r8169 /bin/false" | sudo tee /etc/modprobe.d/r8169-disable.conf && sudo update-initramfs -u -k all && echo "Driver r8169 blacklistado! Reinicie o computador..." || exit 1
 
